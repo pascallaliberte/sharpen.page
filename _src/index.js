4f8feb9d7655fb9c401e3bf8df2182204dd05adc
Pascal Laliberté <pascal@pascallaliberte.me>
2018-11-27 13:41:26 -0500

    Animate tagline on turbolinks transition

--- _src/index.js
+++ _src/index.js
@@ -4,3 +4,39 @@ import Turbolinks from 'turbolinks';
 import './index.scss';
 
 Turbolinks.start();
+
+function pauseClassTransitionForElement(selector, newBody, currentBody) {
+  const newEl = newBody.querySelector(selector);
+  const currentEl = currentBody.querySelector(selector);
+
+  if (newEl.className === currentEl.className) {
+    return;
+  }
+
+  newEl.setAttribute('data-incoming-class', newEl.className);
+  newEl.className = currentEl.className;
+}
+
+function resumeClassTransitionForElement(selector) {
+  const el = document.querySelector(selector)
+
+  if (!el.hasAttribute('data-incoming-class')) {
+    return
+  }
+
+  setTimeout(() => {
+    el.className = el.getAttribute('data-incoming-class')
+    el.removeAttribute('data-incoming-class')
+  }, 200)
+}
+
+document.addEventListener('turbolinks:before-render', (event) => {
+  const { newBody } = event.data
+  const currentBody = document.querySelector('body')
+
+  pauseClassTransitionForElement('[data-behavior="navigation__tagline"]', newBody, currentBody)
+})
+
+document.addEventListener('turbolinks:load', () => {
+  resumeClassTransitionForElement('[data-behavior="navigation__tagline"]')
+})
