f0544f538dd5c085fc2a78d5e24403a6eb8dd561
Pascal Laliberté <pascal@pascallaliberte.me>
2020-10-09 20:43:44 -0400

    review-player: add keyboard navigation

--- _src/controllers/review-player_controller.js
+++ _src/controllers/review-player_controller.js
@@ -17,11 +17,13 @@ export default class extends Controller {
   }
   
   togglePlayback(event) {
-    if (event.target == this.progressbarTarget) { return }
-    if (event.target == this.toggleLayoutButtonTarget) { return }
-    if (event.target == this.sitePreviewFrameTarget) { return }
-    if (event.target == this.sitePreviewURLTarget) { return }
-    if (event.target == this.sitePreviewLoadButton) { return }
+    if (event !== undefined) {
+      if (event.target == this.progressbarTarget) { return }
+      if (event.target == this.toggleLayoutButtonTarget) { return }
+      if (event.target == this.sitePreviewFrameTarget) { return }
+      if (event.target == this.sitePreviewURLTarget) { return }
+      if (event.target == this.sitePreviewLoadButton) { return }
+    }
     
     if (this.element.dataset.status == 'playing') {
       this.pause()
@@ -44,6 +46,22 @@ export default class extends Controller {
     const percentage = value / max
     
     this.videoTarget.currentTime = percentage * this.videoTarget.duration
+  }
+  
+  controlVideoByKeyboard(event) {
+    if (event.code === "Space")      { event.preventDefault(); event.stopPropagation(); this.togglePlayback() }
+    if (event.code === "ArrowLeft")  { event.preventDefault(); event.stopPropagation(); this.skipBack() }
+    if (event.code === "ArrowRight") { event.preventDefault(); event.stopPropagation(); this.skipForward() }
+  }
+  
+  skipBack() {
+    if (this.videoTarget.currentTime < 10) { this.videoTarget.currentTime = 0; return }
+    this.videoTarget.currentTime = this.videoTarget.currentTime - 10;
+  }
+  
+  skipForward() {
+    if (this.videoTarget.duration - this.videoTarget.currentTime < 10) { this.videoTarget.currentTime = this.videoTarget.duration; this.pause(); return }
+    this.videoTarget.currentTime = this.videoTarget.currentTime + 10;
   }
   
   updateComponents(event) {
