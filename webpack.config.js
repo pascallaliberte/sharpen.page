6f197a878506f4db8fa6d847388c0f914c600357
Pascal Laliberté <pascal@hey.com>
2024-03-17 09:13:26 -0400

    fix mode, fix favicons

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,12 @@
-module.exports = function getWebpackConfig(env) {
+module.exports = (env, argv) => {
+  const mode = argv.mode || 'development'; // Fallback to 'development' if mode isn't specified
+  // You can still use `env` for additional environment-specific parameters
+  const configPath = `./config/webpack.${mode}.js`;
+
   // eslint-disable-next-line global-require, import/no-dynamic-require
-  return require(`./config/webpack.${env}.js`);
+  const config = require(configPath);
+
+  // Optionally adjust the config here based on `env` or `argv`
+
+  return config;
 };
