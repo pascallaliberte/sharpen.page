fa3f6074ab49c908ab6e1e159030f8f4ba0625f0
Pascal Laliberté <pascal@hey.com>
2024-03-17 17:56:48 -0400

    fix production build

--- config/webpack.production.js
+++ config/webpack.production.js
@@ -4,6 +4,7 @@ const CommonConfig = require('./webpack.common.js');
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
+const webpack = require('webpack');
 
 module.exports = merge(CommonConfig, {
   output: {
@@ -12,10 +13,11 @@ module.exports = merge(CommonConfig, {
     publicPath: '/assets/',
   },
   plugins: [
-    new CleanWebpackPlugin(),
-    new CopyWebpackPlugin([{
-      from: 'images/'
-    }]),
+    new CleanWebpackPlugin({ cleanOnceBeforeBuildPatterns: ['assets'], verbose: true }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false,
+    }),
     new ImageminPlugin({ test: /\.(jpe?g|png|gif|svg)$/i }),
   ],
 });
