47f2de41555d485a2931c1d16c333312bc4f3edb
Pascal Laliberté <pascal@hey.com>
2024-03-17 08:45:10 -0400

    update webpack.dev.js

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -2,7 +2,6 @@
 const Merge = require('webpack-merge');
 const CommonConfig = require('./webpack.common.js');
 const path = require('path');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const webpack = require('webpack');
 
 module.exports = Merge(CommonConfig, {
@@ -12,45 +11,11 @@ module.exports = Merge(CommonConfig, {
     publicPath: '/assets/',
   },
   devtool: 'inline-source-map',
-  plugins: [
-    new BrowserSyncPlugin(
-      {
-        host: 'localhost',
-        port: 3000,
-        proxy: 'http://localhost:9090',
-        files: ['public', '_src'],
-        snippetOptions: {
-          rule: {
-            match: /<\/head>/i,
-            fn: function (snippet, match) {
-              return snippet + match;
-            }
-          }
-        },
-        open: false
-      },
-      {
-        reload: false,
-        injectCss: true,
-      },
-    ),
-    new webpack.HotModuleReplacementPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        enforce: 'pre',
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-      },
-    ],
-  },
+  plugins: [],
   devServer: {
-    contentBase: [
+    static: [
       path.resolve('public'),
     ],
-    hot: true,
     port: 9090
   },
 });
